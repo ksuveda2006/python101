0b1e2a66e34e1467415ae5bd3e0ff30ef0a9d572
suveda kakarala <suveda999@gmail.com>
2024-04-02 17:16:04 +0000

    datatypes concept

--- 2--datatypes.py
+++ 2--datatypes.py
@@ -45,4 +45,12 @@ sample_string = 'a quick brown fox'
 # endswith
 # find
 # index
-
+string = "Hello World "
+print(string.startswith("H")) # Boolean True
+print(string.endswith("d")) # Boolean True
+print(string.find("o")) # int 4
+print(string.index("o")) # int 4
+print(string.startswith("e")) # Boolean False
+print(string.endswith("H")) # Boolean False
+print(string)
+print(string.endswith(" ")) # Boolean True
